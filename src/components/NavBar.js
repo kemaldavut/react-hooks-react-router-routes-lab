0f3a2e826afe2971eedd3db00b89b43126847e45
Kemal DAVUT <mustafakemaldavut@gmail.com>
2022-08-21 22:57:24 +0300

    Completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,23 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-function NavBar() {
-  return <div>{/*{code here}*/}</div>;
-}
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <NavLink to="/" className="navbar-item">
+        Home
+      </NavLink>
+      <NavLink to="/movies" className="navbar-item">
+        Movies
+      </NavLink>
+      <NavLink to="/directors" className="navbar-item">
+        Directors
+      </NavLink>
+      <NavLink to="/actors" className="navbar-item">
+        Actors
+      </NavLink>
+    </div>
+  );
+};
 
 export default NavBar;
